0cda9c4ab68dc2135983f969c797f19f1a7f2d9a
dimasalvarezel <dimasalvarezel@gmail.com>
2020-08-29 14:41:36 +0700

    project kedua

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layout/main')
+
+@section('title', 'Dimas alvarezel susendi')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10 mx-auto">
+
+            <center>
+                <h1 class="mt-2">Biodata Dimas Alvarezel</h1>
+            </center>
+
+            <center><a href="https://ibb.co/xgXNQ8L"><img src="https://i.ibb.co/xgXNQ8L/MG-9482-3.jpg" alt="MG-9482-3"
+                        border="0" class="my-3"></a></center>
+            <center>
+                <p1>Nama : Dimas Alvarezel</p1>
+            </center>
+            <center>
+                <p2>Kelas : XI RPL 1</p2>
+            </center>
+            <center>
+                <p3>Umur : 15</p3>
+            </center>
+            <center>
+                <p4>Hoby :Rebahan</p4>
+            </center>
+            <center>
+                <p5> ig : <a href="https://www.instagram.com/dimasalvrzl/?hl=en" class="badge badge-primary">
+                            Klik Disini</a></p5>
+            </center>
+
+
+        </div>
+    </div>
+</div>
+@endsection
